8d0f10755d2749b3c11a51c35c990b0fedfff1f2
idocoh <cido15@gmail.com>
2015-12-05 19:58:04 +0200

    add libsvm and writing the dtat for it

---
+++ src/DeepLearning/writeDataForSVM.py
@@ -0,0 +1,39 @@
+import cPickle as pickle
+
+def writeDataToFile(pcikledFilePath, outputFileName,labelNumber=0):
+    with open(pcikledFilePath) as f:
+        ob = pickle.load(f)
+        f.close()
+    params , labels = ob;
+    
+    
+    FILE_NAME = "results/"+outputFileName
+    outputFile = open(FILE_NAME, "w");
+   
+    for exapmle in zip(params,labels):
+        p = exapmle[0];
+        l = exapmle[1];
+        
+        if (labelNumber==0) :
+            labelString = ','.join(map(str, map(int, l)))
+        elif (labelNumber <= l.size):
+            labelString = str(int(l[labelNumber-1]))
+        else:
+            labelString="?"
+            
+        outputFile.write(labelString)
+        
+        index=1;
+        for x in p:
+            outputFile.write(" " + str(index) + ":" + str(x))
+            index+=1
+        
+        outputFile.write("\n")
+          
+
+    
+if __name__ == "__main__":
+    pickName = "C:\\Users\\Ido\\workspace\\ISH_Lasagne\\src\\DeepLearning\\results\\noLearn_50_3_hiddenLayerOutput_0.pickle"
+    writeDataToFile(pickName,"myData.txt")
+
+    
